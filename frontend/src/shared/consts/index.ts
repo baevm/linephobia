54138ec840b4054a18e04b8a47b99df3117d0ff4
baevm <baev2m@gmail.com>
2024-05-06 17:33:57 +0500

    fix url

--- frontend/src/shared/consts/index.ts
+++ frontend/src/shared/consts/index.ts
@@ -1,2 +1,2 @@
-export const STATS_API = import.meta.env.DEV ? 'http://localhost:8080/v1/' : 'http://95.214.9.220:80'
+export const STATS_API = import.meta.env.DEV ? 'http://localhost:8080/v1/' : 'http://95.214.9.220:8080/v1/'
 export const GITHUB_API = 'https://api.github.com'
